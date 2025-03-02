c366c736d763f4a98596341b389328f00baaf394
gsmkev <gsmkev@gmail.com>
2025-03-01 23:50:35 -0300

    feat: Implement authentication forms for sign-in and sign-up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,10 @@
+import AuthForm from "@/components/AuthForm";
+
 function SignIn() {
-  return (
-    <div>SignIn</div>
-  )
+	return (
+		<section className="flex-center size-full max-sm:px-6">
+			<AuthForm type="sign-in" />
+		</section>
+	);
 }
-export default SignIn
+export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,10 @@
+import AuthForm from "@/components/AuthForm";
+
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+	return (
+		<section className="flex-center size-full max-sm:px-6">
+			<AuthForm type="sign-up" />
+		</section>
+	);
 }
-export default SignUp
+export default SignUp;
