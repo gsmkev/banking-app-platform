95428574d6869f8f99017583035b894a3366606c
gsmkev <gsmkev@gmail.com>
2025-03-07 10:43:00 -0300

    feat: Enhance DoughnutChart with dynamic color generation and formatted tooltips

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -9,12 +9,29 @@ function DoughnutChart({ accounts }: DoughnutChartProps) {
 	const names = accounts.map((account) => account.name);
 	const balances = accounts.map((account) => account.currentBalance);
 
+	const generateColor = (name: string) => {
+		const colors = [
+			"#0747b6",
+			"#2265d8",
+			"#2f91fa",
+			"#1e3a8a",
+			"#3b82f6",
+			"#60a5fa",
+			"#93c5fd",
+		];
+		let hash = 0;
+		for (let i = 0; i < name.length; i++) {
+			hash = name.charCodeAt(i) + ((hash << 5) - hash);
+		}
+		const index = Math.abs(hash % colors.length);
+		return colors[index];
+	};
+
 	const data = {
 		datasets: [
 			{
-				label: "Banks",
 				data: balances,
-				backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
+				backgroundColor: names.map((name) => generateColor(name)),
 			},
 		],
 		labels: names,
@@ -27,10 +44,24 @@ function DoughnutChart({ accounts }: DoughnutChartProps) {
 					legend: {
 						display: false,
 					},
+					tooltip: {
+						callbacks: {
+							label: (tooltipItem) => {
+								const value = tooltipItem.raw as number;
+								const formattedValue = new Intl.NumberFormat("en-US", {
+									style: "currency",
+									currency: "USD",
+								}).format(value);
+
+								return `${formattedValue}`;
+							},
+						},
+					},
 				},
 			}}
 			data={data}
 		/>
 	);
 }
+
 export default DoughnutChart;
